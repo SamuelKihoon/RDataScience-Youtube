d3417daba5a68b4571938240d13c13281072262c
daeyoung4352 <daeyoung25@khu.ac.kr>
2022-12-08 15:14:34 +0900

    주석 수정

--- DYgit.R
+++ DYgit.R
@@ -5,10 +5,10 @@
 install.packages('xlsx')
 library(xlsx)
 youtuber <- read.xlsx(file = file.path('C:/Rworks/youtubers.xlsx'),
-                      header=T, sheetName='풍월량', as.data.frame=TRUE,
+                      header=T, sheetName='보물섬', as.data.frame=TRUE,
                       colIndex=c(2:6))
 
-conlist <- unique(youtuber$contents)
+conlist <- unique(youtuber$contents)  #컨텐츠 종류 저장
 
 mean.by.contents <- function(subject) {
   result <- c()
@@ -17,27 +17,28 @@ mean.by.contents <- function(subject) {
                                            subject]))
   }
   return(result)
-}
+}   #컨텐츠별로 subject의 평균 계산 함수
 
 
 # 컨텐츠-조회수 관계(박스플롯)
-unique(youtuber$contents)
-boxplot(views.1000~contents,  #데이터 ~ 그룹 정보
-        data=youtuber,            #데이터가 지정된 자료구조(출처)
+boxplot(views.1000~contents,  
+        data=youtuber,            
         main='컨텐츠별 조회수')
 
 
 # 평균 이상 데이터만 추출
-upper.mean <- c()
+upper.mean <- c()  #조회수가 평균 이상인 컨텐츠 목록
 for(i in 1:length(conlist)){
   if(mean(youtuber[which(youtuber$contents==conlist[i])
                    ,"views.1000"]) >= mean(youtuber$views.1000)) {
     upper.mean <- append(upper.mean, conlist[i])
-  }
+  } #조건의 만족하는 컨텐츠를 목록에 추가
 }
-upperview <- subset(youtuber, contents %in% upper.mean)
-boxplot(views.1000~contents,  #데이터 ~ 그룹 정보
-        data=upperview,            #데이터가 지정된 자료구조(출처)
+upperview <- subset(youtuber, contents %in% upper.mean) 
+#upper.mean에 있는 컨텐츠만 있는 subset 저장
+
+boxplot(views.1000~contents,  
+        data=upperview,            
         main='컨텐츠별 조회수')
 
 # 조회수-좋아요 관계
@@ -54,8 +55,8 @@ barplot(ds, main='좋아요/조회수',
         ylab='비율')
 
 #상위 5개 영상 출력
-view.t5 <- youtuber[order(-youtuber$views.1000),
-                    c('contents','views.1000','likes.100')][1:5,]
+view.t10 <- youtuber[order(-youtuber$views.1000),
+                    c('contents','views.1000','likes.100','likes.rate')][1:10,]
 
-view.t5
+print(view.t10)
 
