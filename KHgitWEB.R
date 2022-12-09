864178fa02ac82459aaf6a1eae369ff03ee3e9c0
SamuelGalaxys <samuelthegalaxys@gmail.com>
2022-12-09 21:12:16 +0900

    :)

--- KHgitWEB.R
+++ KHgitWEB.R
@@ -11,9 +11,9 @@ install.packages('knitr')
 
 #엑셀 읽기
 install.packages('xlsx')
-
-
+ 
 library(xlsx)
+library(ggplot2)
 
 
 
@@ -28,9 +28,14 @@ total <- read.xlsx(file = file.path('/Users/samuelgalaxys/Desktop/Rworks/RDataSc
                    header=T, sheetName='모듬', as.data.frame=TRUE,
                    colIndex=c(3:7))
 
+
  
 
-youtuber$likes.rate <- youtuber$likes/youtuber$views.1000 #좋아요 비율 만들기
+
+
+
+
+youtuber$likes.rate <- (youtuber$likes/youtuber$views.1000)*100 #좋아요 비율 만들기
 conlist <- unique(youtuber$contents)  #컨텐츠 종류 저장
 
 mean.by.contents <- function(subject) {
@@ -53,7 +58,7 @@ con.view.box <- function() {
                      'contents'] #조회수가 가장 높은 컨텐츠 저장
   
   max.mean <- names(vbc)[vbc== max(vbc)] #평균 조회수가 가장 높은 컨텐츠 저장
-  cat('조회수가 가장 높은 영상의 컨텐츠는',max.con,'입니다.')
+  cat('조회수가 가장 높은 영상의 컨텐츠는',max.con,'입니다.','\n')
   cat('평균 조회수가 가장 높은 영상의 컨텐츠는',max.mean,'입니다.')
 }
 
@@ -94,7 +99,7 @@ view.likes.bar <- function() {
   ds <- mean.by.contents('likes.rate')
   barplot(ds, main='좋아요/조회수',
           xlab='컨텐츠',
-          ylab='비율')
+          ylab='비율(%)')
 }
 
 #상위 10개 영상 출력
@@ -103,10 +108,14 @@ top10 <- function() {
                        c('contents','views.1000','likes.100','likes.rate')][1:10,]
   
   print(view.t10)
+  con.num <- table(view.t10$contents)
+  con.mode <- names(con.num)[con.num == max(con.num)]
+  cat('조회수 탑10에 가장 많은 컨텐츠는',con.mode,'입니다.')
 }
 
 #추천 컨텐츠 정하기
 over_10p.list <- conlist[table(youtuber$contents)>=5]
+
 
 
 con.view.box()
