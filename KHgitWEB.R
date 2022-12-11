c4cf90d1d048b9b452ad2c85b3533136ee140f42
SamuelGalaxys <samuelthegalaxys@gmail.com>
2022-12-11 12:28:53 +0900

    한글 오류 일부 수정

--- KHgitWEB.R
+++ KHgitWEB.R
@@ -28,17 +28,14 @@ total <- read.xlsx(file = file.path('/Users/samuelgalaxys/Desktop/Rworks/RDataSc
                    header=T, sheetName='모듬', as.data.frame=TRUE,
                    colIndex=c(3:7))
 
-
- 
-
-youtuber$likes.rate <- (youtuber$likes/youtuber$views.1000)*100 #좋아요 비율 만들기
+youtuber$likes.rate <- (youtuber$likes/youtuber$views.1000)*10 #좋아요 비율 만들기
 conlist <- sort(unique(youtuber$contents))  #컨텐츠 종류 저장
 
 mean.by.contents <- function(subject) {
   result <- c()
   for(i in 1:length(conlist)) {
     result <- append(result, mean(youtuber[which(youtuber$contents==conlist[i]),
-                                           subject]))
+                                           subject])) 
   }
   names(result) <- conlist
   return(result)
@@ -65,7 +62,7 @@ con.view.box_uppermean <- function() {
     if(mean(youtuber[which(youtuber$contents==conlist[i])
                      ,"views.1000"]) >= mean(youtuber$views.1000)) {
       upper.mean <- append(upper.mean, conlist[i])
-    } #조건의 만족하는 컨텐츠를 목록에 추가
+    } #조건에 만족하는 컨텐츠를 목록에 추가
   }
   upperview <- subset(youtuber, contents %in% upper.mean) 
   #upper.mean에 있는 컨텐츠만 있는 subset 저장
@@ -77,15 +74,15 @@ con.view.box_uppermean <- function() {
 
 # 조회수-좋아요 관계
 view.likes.point <- function() {
-  yout.data <- youtuber[, c(2,3,6)] 
+  yout.data <- youtuber[, c(2,3,6)] #조회수와 좋아요 데이터만 추출
   total.data <- total[, c(1,2,5)]
   ggplot(total.data, aes(x=views.1000, y=likes.100))+
     geom_point(color='red')+
-    geom_smooth(method='lm', formula = 'y~x', color='red')+
+    geom_smooth(method='lm', formula = 'y~x', color='red')+ #추세선 추가
     geom_point(data=yout.data, aes(x=views.1000, y=likes.100), color='blue')+
     geom_smooth(data=yout.data, aes(x=views.1000, y=likes.100), color='blue',
                 method='loess', formula = 'y~x')+
-    scale_x_continuous(limits = c(0, max(yout.data$views.1000)))+
+    scale_x_continuous(limits = c(0, max(yout.data$views.1000)))+ #x,y축의 limit을 유튜버 데이터의 최댓값으로 고정
     scale_y_continuous(limits = c(0, max(yout.data$likes.100)))
 }
 
@@ -102,37 +99,116 @@ view.likes.bar <- function() {
 top10 <- function() {
   view.t10 <- youtuber[order(-youtuber$views.1000),
                        c('contents','views.1000','likes.100','likes.rate')][1:10,]
-  
+  #조회수로 정렬후 상위10개 영상의 데이터 저장
   print(view.t10)
   con.num <- table(view.t10$contents)
-  con.mode <- names(con.num)[con.num == max(con.num)]
+  con.mode <- names(con.num)[con.num == max(con.num)] #탑10의 컨텐츠 최빈값
   cat('조회수 탑10에 가장 많은 컨텐츠는',con.mode,'입니다.')
 }
 
-#추천 컨텐츠 정하기
-over_10p.list <- conlist[table(youtuber$contents)>=5]
-over_10p.list
 
 
+
+#예상 수익 비율
 con.return.pie <- function() {
-  tot <- mean.by.contents('likely_return.1000')*table(youtuber$contents)
-  tot <- (tot/sum(tot))*100
-  pie <- data.frame(conlist, tot)[2:3]
+  tot <- mean.by.contents('likely_return.1000')*table(youtuber$contents) #총 예상수익
+  tot <- (tot/sum(tot))*100  # 비율을 %로 저장
+  pie <- data.frame(conlist, tot)[2:3] #데이터 프레임 형성 후 컨텐츠와 예상수익만 저장
   colnames(pie)<- c('contents','tlr')
   ggplot(pie, aes(x='', y=tlr ,fill = factor(conlist))) +
-    geom_bar(stat='identity')+
+    geom_bar(stat='identity')+ #막대 그래프 형성
     theme_void()+
-    coord_polar(theta = "y", start=0)+
-    geom_text(aes(label=paste0(round(tlr,1),'%')),
+    coord_polar(theta = "y", start=0)+ #막대그래프를 원그래프로 전환
+    geom_text(aes(label=paste0(round(tlr,1),'%')), #비중(%)표시
               position=position_stack(vjust=0.5))
 }
+
+# 컨텐츠-좋아요수 관계 (박스플롯)
+con.like.box <- function() {
+  clb <- mean.by.contents('likes.100')
+  boxplot(likes.100~contents,  
+          data=youtuber,            
+          main='컨텐츠별 좋아요수')
+  max.l.con <-youtuber[which(youtuber$likes.100==max(youtuber$likes.100)),
+                       'contents'] #조회수가 가장 높은 컨텐츠 저장
+  
+  max.l.mean <- names(clb)[clb== max(clb)] #평균 좋아요수가 가장 높은 컨텐츠 저장
+  cat('좋아요수가 가장 높은 영상의 컨텐츠는',max.l.con,'입니다.')
+  cat('평균 좋아요수가 가장 높은 영상의 컨텐츠는',max.l.mean,'입니다.')
+}
+
+# 평균 이상 (좋아요수) 데이터만 추출
+con.like.box_uppermean <- function() {
+  upper.l.mean <- c()  #좋아요수가 평균 이상인 컨텐츠 목록
+  for(i in 1:length(conlist)){
+    if(mean(youtuber[which(youtuber$contents==conlist[i])
+                     ,"likes.100"]) >= mean(youtuber$likes.100)) {
+      upper.l.mean <- append(upper.l.mean, conlist[i])
+    } #조건에 만족하는 컨텐츠를 목록에 추가
+  }
+  upperlike <- subset(youtuber, contents %in% upper.l.mean) 
+  #upper.l.mean에 있는 컨텐츠만 있는 subset 저장
+  
+  boxplot(likes.100~contents,  
+          data=upperlike,            
+          main='컨텐츠별 평균 이상 좋아요수')
+}
+
+
+
+#추천 컨텐츠 정하기
+con.rec <- function() {
+  over_10p.list <- conlist[table(youtuber$contents)>=5] #5번 이상 등장한 컨텐츠 저장
+  over_10p.sub <- subset(youtuber, contents %in% over_10p.list) 
+  conlist <- sort(unique(over_10p.sub$contents)) #섭셋의 컨텐츠 리스트로 재설정
+  mean.by.contents <- function(subject) {
+    result <- c()
+    for(i in 1:length(conlist)) {
+      result <- append(result, mean(over_10p.sub[which(over_10p.sub$contents==conlist[i]),
+                                                 subject]))
+    } 
+    names(result) <- conlist
+    return(result)
+  }#섭셋에서 작동하는 함수로 재설정
+  subtlr <- mean.by.contents('likely_return.1000')*table(over_10p.sub$contents) #총 예상수익
+  subv <- mean.by.contents('views.1000') #조회수
+  sublr <- mean.by.contents('likes.rate')  #좋아요 비율
+  
+  score <- c()  #점수를 저장할 벡터
+  tmp.order <- order(subtlr)
+  for(j in 1:length(tmp.order)) {
+    score[tmp.order[j]] <- j*5
+  } #총 예상수익의 등수에 따라 점수 배정
+  tmp.order <- order(subv)
+  for(j in 1:length(tmp.order)) {
+    score[tmp.order[j]] <- score[tmp.order[j]]+ j*5
+  } #평균 조회수 등수에 따라 점수 배정
+  tmp.order <- order(sublr)
+  for(j in 1:length(tmp.order)) {
+    score[tmp.order[j]] <- score[tmp.order[j]]+ j
+  } #좋아요 비율 등수에 따라 점수 배정
+  link <- c()  #컨텐츠의 추천 영상을 저장할 벡터
+  for(i in 1:length(conlist)) {
+    p <- subset(over_10p.sub, contents==conlist[i])
+    link <- append(link, p[which(p$views.1000==max(p$views.1000)),'link'])
+  } #각 컨텐츠별 가장 조회수가 높은 영상의 링크를 저장
+  ds <- as.data.frame(cbind(subtlr, subv, sublr, score,link))
+  colnames(ds) <- c('총 예상수익','평균 조회수','좋아요 비율','추천도','추천 영상 링크')
+  ds[order(ds$추천도, decreasing = TRUE),] #추천도에 따라 정렬후 출력
+}  
+
+
 
 
 con.view.box()
 con.view.box_uppermean()
 view.likes.point()
+con.like.box()
+con.like.box_uppermean()
 view.likes.bar()
 top10()
-youtuber$likely_return.1000
 con.return.pie()
+con.rec()
+
+
 
